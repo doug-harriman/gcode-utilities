092d28fd18796b873de3dd7b8fbf1079737fec17
Doug Harriman <doug.harriman@simplexitypd.com>
2021-12-31 09:23:54 -0800

    Unidirectional slot cutting path generator working.

--- toolpaths.py
+++ toolpaths.py
@@ -8,17 +8,14 @@
 # * For paths and patterns that are simpler to specify with code than CAD.
 #
 
-# TODO: Optional job header - In case you want to have multiple paths in a file
-# TODO: Optional job footer - In case you want to have multiple paths in a file
+# TODO: Add slotting
+# TODO: Make everything relative.  See G-Codes in CNCjs macro for 3-axis tool homing.
 # TODO: Support units
+# TODO: CLI
+# TODO: JSON config?
 
-from enum import Enum, unique
 import numpy as np
-
-@unique
-class Operation(Enum): 
-    CutOut = 1  # Cut shape out.  Tool on outside of boundary.
-    Pocket = 2  # Create a Pocket.  Tool on inside of boundary.
+import math
 
 class ToolPath():
 
@@ -27,23 +24,18 @@ class ToolPath():
     _speed_feed     = 300
     _speed_position = 1500
 
-    _stepover  = 0.5
+    _stepover = 0.5
     _tool_dia = 3.175
-
-    _x = 0.0
-    _y = 0.0
 
     _z_top     =  0.0
     _z_bottom  = -3.0
     _z_retract =  3.0
-    _z_step    = 0.25  # Stepdown between passes
-
-    _operation = Operation.CutOut
+    _stepdown  = 0.25  
 
     _gcode = ''  # Generated G-Code
 
     # Constants
-    RETRACT_CLEARANCE_MIN = 0.5  # [mm]
+    RETRACT_MIN = 0.5  # [mm]
     PLUNGE_N_DIA = 5  # Execute plunge move in N tool diameters
     EOL = '\n'
 
@@ -56,50 +48,17 @@ class ToolPath():
         if self.z_top < self.z_bottom:
             raise ValueError(f"Top height ({self.z_top:03.f}) < bottom height ({self.z_bottom:03.f})")
 
-        if self.z_retract < self.z_top + self.RETRACT_CLEARANCE_MIN:
-            self.z_retract = self.z_top + self.RETRACT_CLEARANCE_MIN
+        if self.z_retract < self.z_top + self.RETRACT_MIN:
+            self.z_retract = self.z_top + self.RETRACT_MIN
             print(f'Warning: retract height adjusted to: {self.z_retract:03.f}')
 
-        if self.z_step > self.z_top - self.z_bottom:
-            self.z_step = self.z_top - self.z_bottom
-            print(f'Warning: Z step was greater than total height, adjusted to: {self.z_step:03.f}')
+        if self.stepdown > self.z_top - self.z_bottom:
+            self.stepdown = self.z_top - self.z_bottom
+            print(f'Warning: Z step was greater than total height, adjusted to: {self.stepdown:03.f}')
 
         # Cutting
         if self.tool_dia < self.stepover:
             raise ValueError(f"Tool diameter ({self.tool_dia:03.f}) is smaller than stepover ({self.stepover:03.f})")
-
-    @property
-    def operation(self) -> Operation:
-        '''
-        Operation type: CutOut or Pocket.
-        '''
-        return self._operation
-
-    @operation.setter
-    def operation(self,value:Operation):
-        self._operation = value
-
-    @property
-    def x(self) -> float:
-        '''
-        X-position for tool path geometry.
-        '''
-        return self._x
-
-    @x.setter
-    def x(self,value:float):
-        self._x = value
-
-    @property
-    def y(self) -> float:
-        '''
-        Y-position for tool path geometry.
-        '''
-        return self._y
-
-    @y.setter
-    def y(self,value:float):
-        self._y = value
 
     @property
     def z_top(self) -> float:
@@ -139,19 +98,19 @@ class ToolPath():
         self._z_retract = value        
     
     @property
-    def z_step(self) -> float:
+    def stepdown(self) -> float:
         '''
         Z step down per pass.
         Stored as a positive value.
         '''
-        return self._z_step
+        return self._stepdown
 
-    @z_step.setter
-    def z_step(self,value:float):
+    @stepdown.setter
+    def stepdown(self,value:float):
         if value < 0:
             value = -value
 
-        self._z_step = value
+        self._stepdown = value
 
     @property
     def stepover(self) -> float:
@@ -262,22 +221,193 @@ class ToolPath():
         self.ParamCheck()
         return self._gcode
 
+class ToolPathSlotUniDi(ToolPath):
+    '''
+    Tool path generation for a slot relative to current position cut in one direction.
+    First pass is cutting pass.  To cut in reverse direcion, set start position to far
+    end and set cut target as a negative position.
+    '''
+
+    _x = 0.0
+    _y = 0.0
+    _depth = 1
+
+    def __init__(self,x:float=0,y:float=1,depth:float=1):
+        super().__init__()
+
+        self.x = x
+        self.y = y
+        self.depth = depth
+
+    @property
+    def x(self) -> float:
+        '''
+        X-position for tool path geometry.
+        '''
+        return self._x
+
+    @x.setter
+    def x(self,value:float):
+        self._x = value
+
+    @property
+    def y(self) -> float:
+        '''
+        Y-position for tool path geometry.
+        '''
+        return self._y
+
+    @y.setter
+    def y(self,value:float):
+        self._y = value
+
+    @property
+    def depth(self) -> float:
+        '''
+        Depth of cut.
+        '''
+        return self._depth
+
+    @depth.setter
+    def depth(self,value:float):
+
+        if value < 0:
+            value = -value
+
+        self._depth = value
+
+    def GCode(self):
+        '''
+        Generate G-Code for unidirectional cut slot tool path.
+        '''
+
+        # Verify parameters
+        self.ParamCheck()
+
+        # Call parent for header.
+        #super().header
+
+        # Prerender
+        x0 = self._to_str(-self.x)
+        y0 = self._to_str(-self.y)
+        x1 = self._to_str(self.x)
+        y1 = self._to_str(self.y)
+
+        fG0 = self._to_str(self.speed_position)
+        fG1 = self._to_str(self.speed_feed)
+
+        # Header
+        self.AddLine('; ---- Job Description ----')
+        self.AddLine('; Unidirectional slot cut')
+        self.AddLine(f'; X Distance: {x1} [mm]')
+        self.AddLine(f'; Y Distance: {y1} [mm]')
+        self.AddLine(f'; Depth     : {self._to_str(self.depth)} [mm]')
+        self.AddLine(f'; Stepdown  : {self._to_str(self.stepdown)} [mm]')
+        self.AddLine('')
+
+        self.AddLine(f'; Feed Speed       : {fG1} [mm/min]')
+        self.AddLine(f'; Positioning Speed: {fG0} [mm/min]')
+        self.AddLine('')
+
+        self.AddLine('; ---- Setup ----')
+        self.AddLine('G21 ; Units: mm')
+        self.AddLine('G94 ; Speed in units per minute')
+        self.AddLine('G17 ; XY Plane')
+        self.AddLine('G54 ; Coordinate system 1')
+        self.AddLine('G91 ; REL positioning mode')
+        self.AddLine('')
+
+        # Start spindle
+        self.AddLine('; Start Spindle')
+        self.AddLine('M3 S5000')  # TODO: Should make this configurable.
+        self.AddLine('G1 P1    ; Wait to spin up')
+        self.AddLine('')
+
+
+        cnt_pass = math.ceil(self.depth / self.stepdown)
+        cur_pass = 0
+
+        # Loop
+        z = self.depth
+        stepdown = self.stepdown
+        while(z > 0):  
+            # Calc next z cut position
+            z -= stepdown
+
+            # Do short stepdown if needed to get to bottom.
+            # If already at zero, we're done.
+            if z < 0:
+                stepdown += z 
+                z = 0
+
+            # Move down to cut position
+            # NOTE: Assumes starting off of workpiece.  No 
+            cur_pass += 1
+            self.AddLine(f'; ---- Pass {cur_pass}/{cnt_pass} ----')
+            self.AddLine('; Cut')
+            self.AddLine(f'G0 Z{self._to_str(-stepdown)}')
+
+            # Cut pass
+            self.AddLine(f'G1 X{x1} Y{y1} F{fG1}')
+            self.AddLine('')
+
+            # Retrace reverse pass
+            if z > 0:
+                self.AddLine('; Retrace')
+                self.AddLine(f'G0 Z{self._to_str(self.z_retract)} F{fG0}')
+                self.AddLine(f'G0 X{x0} Y{y0}')
+                self.AddLine(f'G0 Z{self._to_str(-self.z_retract)} F{fG0}')
+                self.AddLine('')
+
+        # Return to original position
+        self.AddLine('; Cleanup')
+        self.AddLine('M5    ; Stop Spindle')
+        self.AddLine(f'G0 Z{self._to_str(self.depth + self.z_retract)} ; Return Home')
+        self.AddLine(f'G0 X{x0} Y{y0}')
+        self.AddLine(f'G0 Z{self._to_str(-self.z_retract)}')
+        self.AddLine('M2    ; Job end')
+        self.AddLine('')
+
 class ToolPathRectangle(ToolPath):
     '''
     Tool path geneation for a rectangle.
     '''
 
+    _x = 0.0
+    _y = 0.0
+
     _width  = 10.0  # X-width
     _height = 10.0  # Y-height
 
-    def __init__(self,x:float=0,y:float=0,width:float=10,height:float=10,operation:Operation=Operation.Pocket):
+    def __init__(self,x:float=0,y:float=0,width:float=10,height:float=10):
         super().__init__()
 
         self.x = x
         self.y = y
         self.width  = width
         self.height = height
-        self.operation = operation
+
+    @property
+    def x(self) -> float:
+        '''
+        X-position for tool path geometry.
+        '''
+        return self._x
+
+    @x.setter
+    def x(self,value:float):
+        self._x = value
+
+    @property
+    def y(self) -> float:
+        '''
+        Y-position for tool path geometry.
+        '''
+        return self._y
+
+    @y.setter
+    def y(self,value:float):
+        self._y = value
 
     @property
     def width(self) -> float:
@@ -318,8 +448,8 @@ class ToolPathRectangle(ToolPath):
 
         # Generate array of corner positions.
         rt = self.tool_rad
-        if self.operation == Operation.Pocket:
-            rt = -rt
+        # if self.operation == Operation.Pocket:
+        #     rt = -rt
 
         # Base positions with lower left at (0,0)
         positions = np.array([[-rt,                 -rt    ], 
@@ -364,16 +494,16 @@ class ToolPathRectangle(ToolPath):
             length = lengths[idx]
             if length < plunge_dist: 
                 # Won't complete plunge on this side.
-                z[idx] = z[idx-1] - self.z_step * length / plunge_dist
+                z[idx] = z[idx-1] - self.stepdown * length / plunge_dist
                 plunge_dist -= length  # Remaning plunge distance
 
             else:
-                z[idx] = -self.z_step
+                z[idx] = -self.stepdown
 
         # print(z)
 
         # Determine total number of passes
-        n_passes = (self.z_top - self.z_bottom) / self.z_step
+        n_passes = (self.z_top - self.z_bottom) / self.stepdown
         n_passes = int(np.ceil(n_passes))
 
         # Add in an additional pass at the bottom to make sure we get full final depth
@@ -390,7 +520,7 @@ class ToolPathRectangle(ToolPath):
         self.AddLine(f';  position: x:{self._to_str(self.x)}, y:{self._to_str(self.y)}')
         self.AddLine(f';  z top   : {self._to_str(self.z_top)}')
         self.AddLine(f';  z bottom: {self._to_str(self.z_bottom)}')
-        self.AddLine(f';  DOC     : {self._to_str(self.z_step)}')
+        self.AddLine(f';  DOC     : {self._to_str(self.stepdown)}')
         self.AddLine(f';  passes  : {self._to_str(n_passes)}')
         self.AddLine(f';  tool dia: {self._to_str(self.tool_dia)}')
         op = format(self.operation).replace('.',': ')
@@ -435,7 +565,7 @@ class ToolPathRectangle(ToolPath):
                 self.AddLine(f'G1 X{x_str} Y{y_str} Z{z_str}')
 
             # Step down
-            z -= self.z_step
+            z -= self.stepdown
 
             # Cap at bottom
             z[z<self.z_bottom] = self.z_bottom
@@ -450,26 +580,25 @@ class ToolPathRectangle(ToolPath):
 
 if __name__ == '__main__':
 
-    # Cut out laser pattern
-    # Load laser pattern data & calc cutout info
-    import gcode_utils
-    gcu = gcode_utils.GcodeUtils('speed-power-tuning.nc')
-    gcu.TranslateLowerLeft()  # We'll start in lower left, so treat pattern as there.
-    ext = gcu.Extents()
-    w = ext[3]-ext[0]
-    h = ext[4]-ext[1]
+    # # Generate cutout path.
+    # clearance = 1 
+    # tp = ToolPathRectangle(x=ext[0]-clearance, y=ext[1]-clearance, 
+    #                       width=w+2*clearance, height=h+2*clearance, 
+    #                       operation=Operation.CutOut)
+    # tp.tool_dia = 3.175
+    # tp.speed_position = 1500
+    # tp.speed_feed     = 1000
+    # tp.z_top = 0
+    # tp.z_bottom = -6 # Acrylic thickness, not in laser file.
+    # tp.stepdown = 0.25
+    # tp.GCode()
+    # tp.Save('rectangle-test.nc')
 
-    # Generate cutout path.
-    clearance = 1 
-    tp = ToolPathRectangle(x=ext[0]-clearance, y=ext[1]-clearance, 
-                          width=w+2*clearance, height=h+2*clearance, 
-                          operation=Operation.CutOut)
-    tp.tool_dia = 3.175
+    # Slot tool path
+    tp = ToolPathSlotUniDi(y=50, depth=5)
     tp.speed_position = 1500
     tp.speed_feed     = 1000
-    tp.z_top = 0
-    tp.z_bottom = -6 # Acrylic thickness, not in laser file.
-    tp.z_step = 0.25
+    tp.stepdown       = 2
+    tp.z_retract      = 0.5
     tp.GCode()
-    tp.Save('rectangle-test.nc')
-
+    tp.Save('vertical-slot.nc')
