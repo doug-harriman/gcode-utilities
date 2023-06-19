9547ad7776b2971a3854d4efd2ea778c7f4b688f
Doug Harriman <doug.harriman@simplexitypd.com>
2023-06-19 10:19:38 -0700

    Ring job time esitmates, more segments of circle.

--- ring.py
+++ ring.py
@@ -1,31 +1,37 @@
 # Generates G-Code for a ring.
 from toolpaths import ToolPathCylinder, ToolPathFace, AppendFiles
+import datetime as dt
+import os
 
 # Ring
 ID = 18.85
-OD = ID + 3 * 2
-HEIGHT = 5.5
+RING_RADIAL_THICKNESS = 2
+OD = ID + RING_RADIAL_THICKNESS * 2
+HEIGHT = 6
 
 # Tool
 TOOL_DIA = 3.175
 
 # Workpiece
-WORKPIECE_HEIGHT = 19
+WORKPIECE_HEIGHT = 20
 
 # Speeds & feeds
-FEED_RATE = 600
+FEED_RATE = 500
 POS_RATE = 1500
 STEPDOWN = 0.5
-STEPOVER = TOOL_DIA * 0.2
+STEPOVER = 0.25
 
 # Locate center of ring inside workpiece
-x_center = OD / 2 + TOOL_DIA / 2 + STEPOVER
+x_center = OD / 2 + TOOL_DIA + STEPOVER
 y_center = x_center
 
 # File name
 fn_ring = f"ring-id={ID}-od={OD}-h={HEIGHT}.nc"
 with open(fn_ring, "w") as fp:
-    fp.write(f"; Ring ID={ID} OD={OD} H={HEIGHT}\n")
+    fp.write("\n; ---- Operations ----\n\n")
+
+# Total job time
+job_time = dt.timedelta(seconds=0)
 
 # Facing
 if WORKPIECE_HEIGHT > HEIGHT:
@@ -33,7 +39,8 @@ if WORKPIECE_HEIGHT > HEIGHT:
 
     # Create facing toolpath
     side_len = OD + TOOL_DIA + STEPOVER
-    facing = ToolPathFace(x=side_len, y=side_len+7)
+    side_len = 28 + TOOL_DIA
+    facing = ToolPathFace(x=side_len, y=side_len)
     facing.z_top = 0
     facing.z_bottom = -(WORKPIECE_HEIGHT - HEIGHT)
     facing.stepdown = 1.5
@@ -42,6 +49,7 @@ if WORKPIECE_HEIGHT > HEIGHT:
     facing.speed_position = POS_RATE
     facing.tool_dia = TOOL_DIA
     facing.GCode()
+    job_time += facing.estimated_duration
 
     depth = facing.z_top - facing.z_bottom
     fn_facing = f"facing-side={side_len:0.2f}-depth={depth:0.2f}.nc"
@@ -50,6 +58,7 @@ if WORKPIECE_HEIGHT > HEIGHT:
 
 # Cut inner bore
 bore = ToolPathCylinder(x=x_center, y=y_center)
+bore.segments = 120
 bore.bore = True
 bore.diameter = ID
 bore.z_top = -(WORKPIECE_HEIGHT - HEIGHT)
@@ -61,6 +70,8 @@ bore.tool_dia = TOOL_DIA
 bore.stepdown = STEPDOWN
 bore.stepover = STEPOVER
 bore.GCode()
+job_time += bore.estimated_duration
+
 fn_bore = f"bore-id={bore.diameter}.nc"
 bore.Save(fn_bore)
 AppendFiles(fn_ring, fn_bore)
@@ -70,6 +81,18 @@ profile = bore
 profile.bore = False
 profile.diameter = OD
 profile.GCode()
+job_time += profile.estimated_duration
+
 fn_profile = f"profile-id={profile.diameter}.nc"
 profile.Save(fn_profile)
 AppendFiles(fn_ring, fn_profile)
+
+
+# ADd in header
+fn_header = "header.nc"
+with open(fn_header, "w") as fp:
+    fp.write(f"; Ring ID={ID} OD={OD} H={HEIGHT}\n")
+    fp.write(f"; Estimated job time: {job_time}\n")
+
+AppendFiles(fn_header, fn_ring)
+os.rename(fn_header, fn_ring)
